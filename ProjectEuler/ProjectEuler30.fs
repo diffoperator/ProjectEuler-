ba10fdeb53781432badab2c2b0966b155905f8ef
nikhil <nikhil@neuraloverload.(none)>
2010-09-20 13:30:16 +0530

    Solved some more problems (30,35)

--- ProjectEuler/ProjectEuler30.fs
+++ ProjectEuler/ProjectEuler30.fs
@@ -1,3 +1,10 @@
 ﻿module ProjectEuler30
 
+open System
+
+//Problem 30
+let projecteuler30_soln1 = 
+    let sumoffifthpower n = n.ToString() |> Seq.fold (fun acc e -> acc + System.Math.Pow(Double.Parse(e.ToString()), 5.0)) 0.0
+    seq { 1 .. 1000000 } |> Seq.fold (fun acc e -> if (int(sumoffifthpower e)) = e then acc + e else acc) 0
+
 //Problem 39
--- ProjectEuler/Utils.fs
+++ ProjectEuler/Utils.fs
@@ -23,7 +23,7 @@ let ispalindrome l =
 let trianglenumbergen = Seq.unfold ( fun (a,b) -> if a > 1000 then None else Some(a+b+1,(a+b+1, b+1))) (1,1)
 
 let rec factors (n:bigint) (e:bigint) =
-    if n = 1I then [] else if n % e = 0I then (e)::(factors (n/e) e) else (factors n (e+1I));;
+    if n = 1I then [] else if n % e = 0I then (e)::(factors (n/e) e) else (factors n (e+1I))
     
 let isprime n =
     let max = bigint(System.Math.Ceiling(System.Math.Sqrt(float(n))))
@@ -42,4 +42,11 @@ let SumDigits(x:bigint) =
 
 let reverseandadd (n:bigint) = 
     n + (n.ToString().ToCharArray() |> Seq.toList |> List.rev |> List.fold ( fun acc e -> acc*10I + bigint.Parse(e.ToString())) 0I)
-    
+
+let listRotate l = 
+    match l with
+    |h::t -> List.append t [h]
+    |[] -> []
+
+let rec listRotations list times =
+    if times = 0 then [list] else List.append [list] (listRotations (listRotate list) (times - 1))
